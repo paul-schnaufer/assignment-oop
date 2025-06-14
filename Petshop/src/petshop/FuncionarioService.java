3f7d0265447fb52c46861333d48e7e4308dfbdde
Ideraldo Trentini <trentinijunior57@gmail.com>
2025-06-13 22:37:12 -0300

    oi paulo

--- Petshop/src/petshop/FuncionarioService.java
+++ Petshop/src/petshop/FuncionarioService.java
@@ -2,14 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package TeteuBolaGato;
+package petshop;
 
 /**
  *
- * @author Paulo Guilherme
+ * @author trent
  */
-public class java {
-    public static void main(String[]args){
-        System.out.println("Socorro");
-    }
+public class FuncionarioService {
+    
 }
